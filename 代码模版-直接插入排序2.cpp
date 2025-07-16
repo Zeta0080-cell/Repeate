aecfdf1f5dc07177462c0a7bc4e0bde6c473f8d4
Zeta0080-cell <2489889104@qq.com>
2025-07-16 17:29:05 +0800

    Add files via upload

--- 代码模版-直接插入排序2.cpp
+++ 代码模版-直接插入排序2.cpp
@@ -2,41 +2,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int N=1e3;
+int a[N];
 
 void print(int a[],int n)
 {
-	for(int i=0;i<n;i++)
-	{
-		printf("%d ",&a[i]);
-	}
-	printf("\n");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    printf("\n");
 }
 
 void insert(int a[],int n)
 {
-	int key;
-	for(int i=1;i<n;i++)
-	{
-		key=i;
-		int j=i-1;
-		while(j>0&&a[j]>key)
-		{
-			a[j+1]=a[j];
-			j--;
-		}
-		a[j+1]=key;
-	}
-	print(a,n);
+    for(int i=1;i<n;i++)
+    {
+        int key=a[i];
+        int j=i-1;
+        while(j>=0&&a[j]>key)
+        {
+            a[j+1]=a[j];
+            j--;
+        }
+        a[j+1]=key;
+    }
+    print(a,n);
 }
 
 int main()
 {
-	int n;
-	int a[N];
-	for(int i=0;i<n;i++)
-	{
-		scanf("%d",&a[i]);
-	}
-	insert(a,n);
-	return 0;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    insert(a,n);
+    return 0;
 }
+
+
