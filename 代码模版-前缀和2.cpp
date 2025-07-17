76a6510135d86539f20b8ce10e497be7207af73e
Zeta0080-cell <2489889104@qq.com>
2025-07-17 20:30:42 +0800

    Add files via upload

---
+++ 代码模版-前缀和2.cpp
@@ -0,0 +1,23 @@
+//代码模版-前缀和2
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e3;
+
+int main()
+{
+	int s[N]={0};//用于存储前缀和数组
+	int a[N];
+	int n,m,l,r;
+	cin>>n>>m;
+	for(int i=1;i<=n;i++)
+	{
+		scanf("%d",&a[i]);
+		s[i]=s[i-1]+a[i];
+	}
+	while(m--)
+	{
+		scanf("%d%d",&l,&r);
+		printf("%d",s[r]-s[l-1]);
+	}
+	return 0;
+}
