cc624933b14f3afb9834b2b2bf9c4b472e61cae5
Zeta0080-cell <2489889104@qq.com>
2025-07-20 23:28:49 +0800

    Add files via upload

---
+++ 代码模版-高精度加法2.cpp
@@ -0,0 +1,43 @@
+//代码模版-高精度加法2
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> add(vector<int> &A,vector<int> &B)
+{
+	if(A.size()<B.size())
+	{
+		return add(B,A);
+	}
+	vector<int> C;
+	int t=0;
+	for(int i=0;i<A.size();i++)
+	{
+		t+=A[i];
+		if (i < B.size()) t += B[i];
+		C.push_back(t%10);
+		t /= 10;//判断t是0还是1 
+	} 
+	if (t) C.push_back(t);
+    return C;
+}
+
+int main()
+{
+	string a,b;
+	vector<int>A,B;
+	cin>>a>>b;//获得字符串 
+	for(int i=a.size()-1;i>=0;i--)
+	{
+		A.push_back(a[i]-'0');		
+	} 
+	for(int i=b.size();i>=0;i--)
+	{
+		B.push_back(b[i]-'0');
+	}
+	vector<int> c=add(A,B);
+	for(int i=c.size()-1;i>=0;i--)
+    {
+        printf("%d",c[i]);
+    }
+    return 0;
+} 
