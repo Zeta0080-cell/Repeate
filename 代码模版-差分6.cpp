eebe74fd0053f42847c8a46cda12438511371ae5
Zeta0080-cell <2489889104@qq.com>
2025-08-03 12:48:02 +0800

    Add files via upload

---
+++ 代码模版-差分6.cpp
@@ -0,0 +1,35 @@
+//代码模版-差分6 
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e6;
+int a[N],d[N];
+
+int main()
+{
+	int n,m;
+	int l,r,c;
+	cin>>n>>m;
+	for(int i=1;i<=n;i++)
+	{
+		scanf("%d",&a[i]); 
+	}
+	for(int i=1;i<=n;i++)//构造差分数组 
+	{
+		d[i]=a[i]-a[i-1];	
+	}
+	while(m--)
+	{
+		cin>>l>>r>>c;
+		d[l]+=c;
+		d[r+1]-=c;
+	}
+	for(int i=1;i<=n;i++)//利用前缀和求操做完后的数组 
+	{
+		d[i]+=d[i-1];
+	}
+	for(int i=1;i<=n;i++)
+	{
+		printf("%d",a[i]);
+	}
+	return 0;
+} 
